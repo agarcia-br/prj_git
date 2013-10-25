34e0be998fa0541520cb31a4ea1f584cd02d1ddb
unknown <agarcia@DES011.in.appi.com.br>
2013-10-25 17:57:34 -0200

    Incluindo novos padroes finais

--- dev_p3l/src/layer3/FindSequence.java
+++ dev_p3l/src/layer3/FindSequence.java
@@ -7,6 +7,9 @@ import java.util.Set;
 public class FindSequence {
 	
 	int depthLimit;
+	
+	StructuredPermL3  identity;
+	StructuredPermL3  initialRotation;
 	Set<StructuredPermL3> perms;
 	Set<StructuredPermL3> bottomTurns;
 	
@@ -25,52 +28,54 @@ public class FindSequence {
 	
 	public void initSearchedPatterns() {
 		
-		// 1/12
-		byte [] ident   = new    byte[] {    0,    1,     2,    3,     4,     5,     6,     7};
-		// 1/12 
-		byte [] r_adj   = new    byte[] {    1,    0,     3,    2,     4,     5,     6,     7};
-		// 1/12 
-		byte [] l_adj   = new    byte[] {    3,    1,     2,    0,     4,     5,     6,     7};
-		// 1/12
-		byte [] oposite = new    byte[] {    2,    3,     0,    1,     4,     5,     6,     7};
-		// 1/12 * 4 * 2 = 8/12
-		byte [] cicle   = new    byte[] {    0,    3,     1,    2,     4,     5,     6,     7};
-
+		String [] permname = new String[]{"ident","r_adj","l_adj","oposite","cicle"};
+		byte [][] perm = {
+			// 1/12
+			/* byte [] ident =*/ new    byte[] {    0,    1,     2,    3,     4,     5,     6,     7},
+			// 1/12 
+			/* byte [] r_adj =*/ new    byte[] {    1,    0,     3,    2,     4,     5,     6,     7},
+			// 1/12 
+			/* byte [] l_adj =*/ new    byte[] {    3,    1,     2,    0,     4,     5,     6,     7},
+			// 1/12
+			/* byte [] oposite =*/ new    byte[] {    2,    3,     0,    1,     4,     5,     6,     7},
+			// 1/12 * 4 * 2 = 8/12
+			/* byte [] cicle =*/ new    byte[] {    0,    3,     1,    2,     4,     5,     6,     7}
+		};
+		String [] flipname = new String[]{"noflip","allflip","flip_01","flip_12","flip_23","flip_30","vert_flip","hori_flip"};
+		boolean [][] flip = {
 		// 1/8
-		boolean [] noflip =  new boolean[] { false, false, false, false, false, false, false, false};
+		/*boolean [] noflip =*/  new boolean[] { false, false, false, false, false, false, false, false},
 		// 1/8
-		boolean [] allflip =  new boolean[] { true, true, true, true, false, false, false, false};
+		/*boolean [] allflip =*/  new boolean[] { true, true, true, true, false, false, false, false},
 		// 4/8
-		boolean [] flip_01 =  new boolean[] { true, true, false, false, false, false, false, false};
-		boolean [] flip_12 =  new boolean[] { false, true, true, false, false, false, false, false};
-		boolean [] flip_23 =  new boolean[] { false, false, true, true, false, false, false, false};
-		boolean [] flip_30 =  new boolean[] { true, false, false, true, false, false, false, false};
+		/*boolean [] flip_01 =*/  new boolean[] { true, true, false, false, false, false, false, false},
+		/*boolean [] flip_12 =*/  new boolean[] { false, true, true, false, false, false, false, false},
+		/*boolean [] flip_23 =*/  new boolean[] { false, false, true, true, false, false, false, false},
+		/*boolean [] flip_30 =*/  new boolean[] { true, false, false, true, false, false, false, false},
 		// 1/8 
-		boolean [] vert_flip =  new boolean[] { true, false, true, false, false, false, false, false};
+		/*boolean [] vert_flip =*/  new boolean[] { true, false, true, false, false, false, false, false},
 		// 1/8 
-		boolean [] hori_flip =  new boolean[] { false, true, false, true, false, false, false, false};
-		
+		/*boolean [] hori_flip =*/  new boolean[] { false, true, false, true, false, false, false, false}
+		};
 		StructuredPermL3 p;
-		p = new StructuredPermL3(cicle,noflip, 0,"cicle;noflip(1/8)");
-		finalPatterns.add(p);
-		p = new StructuredPermL3(cicle,allflip, 0,"cicle;allflip(1/8)");
-		finalPatterns.add(p);
-		p = new StructuredPermL3(cicle,flip_01, 0,"cicle;flip_01(1/8)");
-		finalPatterns.add(p);
-		p = new StructuredPermL3(cicle,flip_12, 0,"cicle;flip_12(1/8)");
-		finalPatterns.add(p);
-		p = new StructuredPermL3(cicle,flip_23, 0,"cicle;flip_23(1/8)");
-		finalPatterns.add(p);
-		p = new StructuredPermL3(cicle,flip_30, 0,"cicle;flip_23(1/8)");
-		finalPatterns.add(p);
-		p = new StructuredPermL3(cicle,vert_flip, 0,"cicle;vert_flip(1/8)");
-		finalPatterns.add(p);
-		
-		p = new StructuredPermL3(cicle,hori_flip, 0,"cicle;hori_flip(1/8)");
-		finalPatterns.add(p);
+		for (int i=0; i<5;++i) {
+			for (int j=0; j<8;++j) {
+				p = new StructuredPermL3(perm[i],flip[j], 0,permname[i]+";"+flipname[j]);
+				finalPatterns.add(p);
+			}
+		}
+
 	}
 	
 	public void initPerms() {
+		
+		identity =  new StructuredPermL3(new    byte[] {    0,    1,     2,    3,     4,     5,     6,     7},
+			 	 new boolean[] { false, false, false, false, false, false, false, false},
+				0, "");
+		initialRotation =  new StructuredPermL3(new    byte[] {    3,    0,     1,    2,     7,     4,     5,     6},
+			 	 new boolean[] { false, false, false, false, false, false, false, false},
+				0, ">");
+ 
 		
 		StructuredPermL3 p1;
 		p1 = new StructuredPermL3(new    byte[] {    3,    0,     2,    4,     1,     5,     6,     7},
@@ -121,6 +126,7 @@ public class FindSequence {
 		}
 		return true;
 	}
+
 	
 	public void find(int depth, State s) {
 		if (depth == depthLimit ) {
@@ -128,29 +134,34 @@ public class FindSequence {
 		} else {
 			Set<StructuredPermL3> operations = (depth%2)==0?perms:bottomTurns;
 			
-			for(StructuredPermL3 p : operations) {
-				State novo = p.act(s);
-				Set<StructuredPermL3> found  = new HashSet<StructuredPermL3>();
-				for(StructuredPermL3 fp :finalPatterns) {
-					if (match(novo,fp)) {
-						System.out.println("found "+fp.name+"="+novo.trace);
-						found.add(fp);
+				//State novo = identity.act(s);
+				for(StructuredPermL3 p : operations) {
+					State novo = p.act(s);
+					Set<StructuredPermL3> found  = new HashSet<StructuredPermL3>();
+					for(StructuredPermL3 fp :finalPatterns) {
+						if (match(novo,fp)) {
+							System.out.println("found "+fp.name+"="+novo.trace);
+							found.add(fp);
+						}
 					}
+					finalPatterns.removeAll(found);
+					find(depth+1,novo);
 				}
-				finalPatterns.removeAll(found);
-				find(depth+1,novo);
-			}
 		}
 	}
 
 
 	public static void main (String []args) {
 		
-		System.out.println("###="+(-4 %4));
+		System.out.println("Achadas:");
 		for (int j=3; j<=10; ++j) {
 			System.out.println("\n Achando seq de tamanho "+j);
 			FindSequence fs = new FindSequence(j);
 			fs.find(0,State.getInitialState());
+		}
+		System.out.println("Nao achadas:");
+		for(StructuredPermL3 fp :finalPatterns) {
+			System.out.println("not found "+fp.name);
 		}
 	}
 	
